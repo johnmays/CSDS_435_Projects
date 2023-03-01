2bf07164519ae3f4c246f1a96f58cf9510c071c7
John Mays <25927827+johnmays@users.noreply.github.com>
2023-03-01 17:52:42 -0500

    outline of ff code in

--- project1/feedforward.py
+++ project1/feedforward.py
@@ -1,17 +1,22 @@
 from classifier import Classifier
+from sklearn.neural_network import MLPClassifier
 
 import numpy as np
 
 class FeedForwardNN(Classifier):
     def __init__(self):
-        pass
+        self.net = None
 
     @property
     def name(self):
         return "Feed-forward Neural Network"
 
     def train(self, training_data: np.ndarray, training_labels: np.ndarray) -> None:
-        pass
+        print("Fitting Feed Forward Net...")
+        self.net = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+        self.net.fit(training_data, training_labels)
+        print("Finished!")
+
 
     def infer(self, testing_data: np.ndarray) -> np.ndarray:
         pass
