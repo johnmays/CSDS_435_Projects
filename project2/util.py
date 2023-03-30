49e9f8d2fac1759e319f07e350896a9b513ae88b
John Mays <25927827+johnmays@users.noreply.github.com>
2023-03-29 23:21:55 -0400

    implemented stopwords + data summary

--- project2/util.py
+++ project2/util.py
@@ -4,16 +4,20 @@ Utility functions for CSDS 435 Project 2.
 import numpy as np
 from typing import Tuple
 import matplotlib.pyplot as plt
+import re
 
-def load_data(path: str): 
+stopwords = ["a", "an", "the", "is", "to", "for", "in", "of", "and", "on", '']
+
+def load_data(path: str, stopwords = ["a", "the", "is"]): 
     print('Importing data...')
     # format file into a list of tweets:
     lines = []
     with open(path, newline='\n') as file:
         lines = file.readlines()
-    # take the last column in a line (the tweet) and split it by spaces:
+    # take last column, take out punctuation, make lowercase, split into list of words
     for i, line in enumerate(lines):
-        lines[i] = line.split('|')[2].split(' ')
+        words_in_a_line = re.sub(r'!|:|,|\.|\|', '', line.split('|')[2]).lower().split(' ')
+        lines[i] = remove_stopwords(words_in_a_line, stopwords)
     vocab = np.unique(np.concatenate(lines).flat)
     # creating data vector X:
     n = np.size(vocab)
@@ -27,3 +31,16 @@ def load_data(path: str):
 
     print('X has {} examples and {} features in BOW format...'.format(m,n))
     return X, np.array(vocab)
+
+def remove_stopwords(words, stopwords):
+    return list(filter(lambda word: word not in stopwords, words))
+
+def generate_statistics(X,vocab):
+    print("Dataset Statistics:")
+    m,n = np.shape(X)
+    num_words = int(np.sum(X))
+    print("# Number of Tweets: {}".format(m))
+    print("# Number of Words (Total): {}".format(num_words))
+    print("# Number of Tokens: {}".format(n))
+    print("# Average Number of Words per Tweet: {}".format(round((num_words/m),3)))
+    print("# Top 5 Legal Tokens: {}".format(vocab[np.flip(np.argsort(np.sum(X, axis=0)))[0:5]]))
