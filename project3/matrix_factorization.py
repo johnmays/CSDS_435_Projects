0faf997bae09d6fcbefd9d02e220bea027a45dfe
Aaron3154518 <aaoren210@gmail.com>
2023-04-29 19:34:12 -0400

    Implemented consistent cross fold validation

---
+++ project3/matrix_factorization.py
@@ -0,0 +1,35 @@
+import numpy
+
+"""
+@INPUT:
+    R     : a matrix to be factorized, dimension N x M
+    P     : an initial matrix of dimension N x K
+    Q     : an initial matrix of dimension M x K
+    K     : the number of latent features
+    steps : the maximum number of steps to perform the optimisation
+    alpha : the learning rate
+    beta  : the regularization parameter
+@OUTPUT:
+    the final matrices P and Q
+"""
+def matrix_factorization(R, P, Q, K, steps=5000, alpha=0.0002, beta=0.02):
+    Q = Q.T
+    for step in range(steps):
+        for i in range(len(R)):
+            for j in range(len(R[i])):
+                if R[i][j] > 0:
+                    eij = R[i][j] - numpy.dot(P[i,:],Q[:,j])
+                    for k in range(K):
+                        P[i][k] = P[i][k] + alpha * (2 * eij * Q[k][j] - beta * P[i][k])
+                        Q[k][j] = Q[k][j] + alpha * (2 * eij * P[i][k] - beta * Q[k][j])
+        eR = numpy.dot(P,Q)
+        e = 0
+        for i in range(len(R)):
+            for j in range(len(R[i])):
+                if R[i][j] > 0:
+                    e = e + pow(R[i][j] - numpy.dot(P[i,:],Q[:,j]), 2)
+                    for k in range(K):
+                        e = e + (beta/2) * (pow(P[i][k],2) + pow(Q[k][j],2))
+        if e < 0.001:
+            break
+    return P, Q.T
--- project3/util.py
+++ project3/util.py
@@ -2,11 +2,51 @@
 Utility functions for CSDS 435 Project 3.
 """
 
+from collections import namedtuple
 import numpy as np
 import matplotlib.pyplot as plt
+from surprise import accuracy
+from surprise.model_selection import KFold
 
-purples = ["#0a0612", "#392249", "#482980", "#673ab7",
-           "#7a52aa", "#9779bd", "#b59fd0", "#d3c5e3"]
+purples = [
+    "#0a0612",
+    "#392249",
+    "#482980",
+    "#673ab7",
+    "#7a52aa",
+    "#9779bd",
+    "#b59fd0",
+    "#d3c5e3",
+]
+
 
 def load_data(path: str):
     pass
+
+
+Results = namedtuple("Results", "rmse mae")
+
+
+def run(algo, data) -> Results:
+    res = Results([], [])
+    for trainset, testset in KFold(n_splits=5, random_state=666).split(data):
+        # train and test algorithm.
+        algo.fit(trainset)
+        predictions = algo.test(testset)
+
+        # Compute and print Root Mean Squared Error
+        res.rmse.append(accuracy.rmse(predictions, verbose=False))
+        res.mae.append(accuracy.mae(predictions, verbose=False))
+    return res
+
+
+def display(res: Results):
+    rows = ["RMSE", "MAE"]
+    cols = [""] + [f"Fold {i}" for i in range(len(res.rmse))]
+    col_w = max([len(s) for s in rows + cols])
+    cols = [c + " " * (col_w - len(c)) for c in cols]
+    print(" |".join(cols))
+    for r, d in zip(rows, [res.rmse, res.mae]):
+        d = [r] + [str(int(d * 1e3) / 1e3) for d in d]
+        d = [d + " " * (col_w - len(d)) for d in d]
+        print(" |".join(d))
