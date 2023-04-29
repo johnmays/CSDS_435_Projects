694cfb7d6bb759355c6d14b89583bb7cf172a133
John Mays <25927827+johnmays@users.noreply.github.com>
2023-04-29 19:43:21 -0400

    more complete comparison file

--- project3/generate_results.py
+++ project3/generate_results.py
@@ -11,7 +11,11 @@ import matplotlib.pyplot as plt
 purples = ["#0a0612", "#392249", "#482980", "#673ab7",
            "#7a52aa", "#9779bd", "#b59fd0", "#d3c5e3"]
 
-from util import load_data
+from util import load_data, run, display
+from surprise import Dataset
+from surprise.model_selection import KFold
+
+num_folds = 5
 
 from surprise import NormalPredictor as Random
 from surprise import SVD
@@ -35,18 +39,38 @@ if __name__ == '__main__':
     parser.add_argument('path', metavar='PATH', type=str, help='The path to the data(should be .txt).')
     parser.add_argument('--tuning', dest='tuning', action='store_true',
                         help='turns on tuning, which will loop through the algos many times to evaluate given hyperparameters.')
-    parser.set_defaults(tuning=False)
+    parser.add_argument('--no-cv', dest='cv', action='store_true', help='Turns off five-fold cross-validation split')
+    parser.set_defaults(tuning=False, cv=True)
     args = parser.parse_args()
 
     data_path = os.path.expanduser(args.path)
     tuning = args.tuning
-    
-    # load data
+    cv = args.cv
 
+    # load data
+    data = Dataset.load_builtin('ml-100k')
+
+    # potentially split data
+    folds = []
+    kf = KFold(n_splits=num_folds)
+    if cv:
+        for trainset, testset in kf.split(data):
+            folds.append((trainset, testset))
+    else:
+        kf = KFold(n_splits=num_folds)
+        trainset, testset = next(kf.split(data)) # takes the first element from the generator
+        folds.append((trainset, testset))
+    
     # warnings.filterwarnings("ignore") # WILL SUPRESS WARNING FOR CLEARER OUTPUT
     for algorithm, name in zip(predictors, names):
         predictor = algorithm()
         print('================')
         print(f'{name}:')
         print('================')
-        # print statistics for algo
+        for fold in folds:
+            trainset, testset = fold
+            # print statistics for this fold and this algo
+            print(f'Fold {fold}: ')
+            results = run(predictor, trainset)
+            display(results)
+
