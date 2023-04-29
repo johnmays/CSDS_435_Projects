ebe7fb861b250e69323bcff1945f107fa235c8c6
John Mays <25927827+johnmays@users.noreply.github.com>
2023-04-29 18:32:19 -0400

    figuring out how to import the data

---
+++ project3/util.py
@@ -0,0 +1,9 @@
+"""
+Utility functions for CSDS 435 Project 3.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+purples = ["#0a0612", "#392249", "#482980", "#673ab7",
+           "#7a52aa", "#9779bd", "#b59fd0", "#d3c5e3"]
