3c15858f87efdac21b0e1eb6327dd40bfeff580e
John Mays <25927827+johnmays@users.noreply.github.com>
2023-04-30 16:02:32 -0400

    neural net trains successfully

--- project3/neuralnetwork.py
+++ project3/neuralnetwork.py
@@ -1,8 +1,8 @@
-from predictor import Predictor
+# from predictor import Predictor
 
 import numpy as np
 import keras
-
+"""
 class NNPredictor(Predictor):
     def __init__(self) -> None:
         self.model = None
@@ -12,19 +12,9 @@ class NNPredictor(Predictor):
         return "Neural Network"
     
     def train(self, ratings:np.ndarray) -> bool:
-        """
-        train the recommender
-
-        Args:
-            ...
-        """
         pass
 
     def predict(self, user, movie):
-        """
-        Get a rating for a new pair
-
-        Args:
-            ...
-        """
         pass
+"""
+
