6cade66de21adc277941e62440e3c85516d24f3c
John Mays <25927827+johnmays@users.noreply.github.com>
2023-04-30 16:56:54 -0400

    formalized NN codee + util.py has run_nn method (functional) now

--- project3/neuralnetwork.py
+++ project3/neuralnetwork.py
@@ -18,3 +18,24 @@ class NNPredictor(Predictor):
         pass
 """
 
+def create_model(num_users, num_movies, num_factors):
+    # two input layers for movies and users
+    user_input = keras.layers.Input(shape=(1,))
+    movie_input = keras.layers.Input(shape=(1,))
+
+    # two embedding layers for movies and users (transforms indices into dense vectors)
+    user_embedding = keras.layers.Reshape((num_factors,))(keras.layers.Embedding(num_users,num_factors,embeddings_initializer='he_normal', embeddings_regularizer = keras.regularizers.l2(1e-16))(user_input))
+    movie_embedding = keras.layers.Reshape((num_factors,))(keras.layers.Embedding(num_movies,num_factors,embeddings_initializer='he_normal', embeddings_regularizer = keras.regularizers.l2(1e-16))(movie_input))
+
+    # dense hidden layer
+    # ...
+    
+    # output layer
+    out = keras.layers.Dot(axes=1)([user_embedding, movie_embedding])
+    
+    # Assembling & compiling model
+    model = keras.models.Model(inputs = [user_input,movie_input], outputs=out)
+    adam = keras.optimizers.Adam(lr=1e-3)
+    model.compile(loss='mean_squared_error', optimizer=adam)
+
+    return model
