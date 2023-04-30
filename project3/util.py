301abb76c6b768efbe4189ecfaf8b809ab0ba2cf
Aaron3154518 <aaoren210@gmail.com>
2023-04-30 14:48:23 -0400

    Implemented matrix factorization

--- project3/util.py
+++ project3/util.py
@@ -5,7 +5,9 @@ Utility functions for CSDS 435 Project 3.
 from collections import namedtuple
 import numpy as np
 import matplotlib.pyplot as plt
+from matrix_factorization import matrix_factorization
 from surprise import accuracy
+from surprise.prediction_algorithms.predictions import Prediction
 from surprise.model_selection import KFold
 
 purples = [
@@ -24,26 +26,40 @@ def load_data(path: str):
     pass
 
 
-Results = namedtuple("Results", "rmse mae")
-
-
 def fold(data):
     return KFold(n_splits=5, random_state=666).split(data)
 
 
-def run(algo, data) -> Results:
-    res = Results([], [])
+Index = namedtuple("Index", "uid iid")
+
+
+# Run a surprise algorithm on the data
+def run(algo, data):
+    res = []
     for trainset, testset in fold(data):
         # train and test algorithm.
         algo.fit(trainset)
-        predictions = algo.test(testset)
-
-        # Compute and print Root Mean Squared Error
-        res.rmse.append(accuracy.rmse(predictions, verbose=False))
-        res.mae.append(accuracy.mae(predictions, verbose=False))
+        res.append(algo.test(testset))
     return res
 
 
+# Display the results of a surprise algorithm
+def display(res):
+    rows = ["RMSE", "MAE"]
+    data = [
+        [foo(r, verbose=False) for r in res] for foo in [accuracy.rmse, accuracy.mae]
+    ]
+    cols = [""] + [f"Fold {i}" for i in range(len(res))]
+    col_w = max([len(s) for s in rows + cols])
+    cols = [c + " " * (col_w - len(c)) for c in cols]
+    print(" |".join(cols))
+    for r, d in zip(rows, data):
+        d = [r] + [str(int(d * 1e3) / 1e3) for d in d]
+        d = [d + " " * (col_w - len(d)) for d in d]
+        print(" |".join(d))
+
+
+# Convert data folds into rating matrices
 def get_fold_matrices(data):
     Rs = []
     for trainset, testset in fold(data):
@@ -54,13 +70,25 @@ def get_fold_matrices(data):
     return Rs
 
 
-def display(res: Results):
-    rows = ["RMSE", "MAE"]
-    cols = [""] + [f"Fold {i}" for i in range(len(res.rmse))]
-    col_w = max([len(s) for s in rows + cols])
-    cols = [c + " " * (col_w - len(c)) for c in cols]
-    print(" |".join(cols))
-    for r, d in zip(rows, [res.rmse, res.mae]):
-        d = [r] + [str(int(d * 1e3) / 1e3) for d in d]
-        d = [d + " " * (col_w - len(d)) for d in d]
-        print(" |".join(d))
+# Get predictions for test datasets on matrix factorizations
+def run_mf(data, K, **kwargs):
+    Rs = get_fold_matrices(data)
+    Ps = [np.random.rand(len(R), K) for R in Rs]
+    Qs = [np.random.rand(len(R[0]), K) for R in Rs]
+
+    for i in range(len(Rs)):
+        Ps[i], Qs[i] = matrix_factorization(Rs[i], Ps[i], Qs[i], K, **kwargs)
+    Rs = [np.dot(P, Q.T) for P, Q in zip(Ps, Qs)]
+
+    res = []
+    for R, (trainset, testset) in zip(Rs, fold(data)):
+        pred = []
+        for uid, iid, r in testset:
+            try:
+                i_uid = trainset.to_inner_uid(uid)
+                i_iid = trainset.to_inner_iid(iid)
+                pred.append(Prediction(uid, iid, r, R[i_uid][i_iid], {}))
+            except ValueError:
+                pass
+        res.append(pred)
+    return res
